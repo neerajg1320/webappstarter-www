6abb7747a51ce4802932a14f60b094973b296948
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-08-12 15:21:48 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Wep Application Starter`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
