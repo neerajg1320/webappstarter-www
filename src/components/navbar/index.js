e2d935f054b8c14df9a68210b033b81ef8c3b63d
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-08-14 12:58:10 +0530

    Added the support for links to the web application named studio

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as styles from './styles.module.scss';
 import Container from "../container";
+import {studioPlaygroundUrl, studioUrl} from "../../config/global";
 
 const Navbar = () => {
   console.log(`Navbar render:`, styles);
@@ -12,9 +13,9 @@ const Navbar = () => {
           <div className={styles.navbarLeft}>
             <a className={styles.logo} href="#">WEBAPP STARTER</a>
             <ul className={styles.list}>
-              <li className={styles.listItem}><a href="#">About</a></li>
-              <li className={styles.listItem}><a href="#">Solutions</a></li>
-              <li className={styles.listItem}><a href="#">Work</a></li>
+              <li className={styles.listItem}><a href={`${studioPlaygroundUrl}`}>Playground</a></li>
+              <li className={styles.listItem}><a href={`${studioUrl}`}>Studio</a></li>
+              <li className={styles.listItem}><a href="#">FAQ</a></li>
             </ul>
           </div>
           <a className={styles.cta} href="#">Contact</a>
---
+++ src/config/global.js
@@ -0,0 +1,2 @@
+export const studioUrl = `https://studio.webappstarter.com`;
+export const studioPlaygroundUrl = `https://studio.webappstarter.com/playground`;
